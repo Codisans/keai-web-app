a6e677bc617a627414eb48f9baf336586cda2626
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-09-08 19:14:11 +0100

    fix: add max widrth

--- src/app/(auth)/AuthCard.js
+++ src/app/(auth)/AuthCard.js
@@ -1,7 +1,7 @@
 import { Logo } from '@/components/atoms/Logo'
 
 const AuthCard = ({ children }) => (
-    <div className="py-header px-gg flex flex-col items-center rounded-ui gap-y-6">
+    <div className="py-header px-gg flex flex-col items-center rounded-ui gap-y-6 mx-auto max-w-md">
         <Logo className="[font-size:3rem]" type="app" />
         <div className="w-full p-gg">{children}</div>
     </div>
