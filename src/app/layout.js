f9e0edc4d4259e89a0729eb7d63363e989430dcb
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-09-08 19:06:20 +0100

    fix: add max h screen small to global layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 const RootLayout = ({ children }) => {
     return (
         <html className="overscroll-none" lang="es">
-            <body className="w-full h-screen overflow-y-auto font-sans">
+            <body className="w-full max-h-screen-small h-screen-small overflow-y-auto font-sans">
                 <AppContextProvider>{children}</AppContextProvider>
                 <SpeedInsights />
             </body>
