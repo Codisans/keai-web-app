bdbd28d5fa7360fbdc12442bfe5a648a08f9df72
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-09-08 17:31:04 +0100

    feat: add vercel page insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import '@/app/global.css'
 import { AppContextProvider } from './AppContext'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const RootLayout = ({ children }) => {
     return (
         <html className="overscroll-none" lang="es">
             <body className="w-full h-screen overflow-y-auto font-sans">
                 <AppContextProvider>{children}</AppContextProvider>
+                <SpeedInsights />
             </body>
         </html>
     )
