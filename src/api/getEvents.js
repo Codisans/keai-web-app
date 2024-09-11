b9b70305c9a2991294f6232066368af8befa5c18
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-09-11 22:32:32 +0100

    fix: add patch

--- src/api/getEvents.js
+++ src/api/getEvents.js
@@ -4,6 +4,7 @@ export const getEvents = async category => {
     const res = await fetch(url)
 
     if (!res.ok) {
+        return { data: [] }
         // This will activate the closest `error.js` Error Boundary
         throw new Error('Failed to fetch data')
     }
