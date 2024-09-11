ddc4d56da893f5dff2367417d3356ac8000d611a
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-09-11 21:00:41 +0100

    fix: undo scroll hack

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
-'use client'
 import '@/app/global.css'
 import { AppContextProvider } from './AppContext'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import { useEffect } from 'react'
 
 const RootLayout = ({ children }) => {
-    useEffect(() => {
-        window.scrollTo(0, 1)
-    }, [])
     return (
         <html className="overscroll-none select-none" lang="es">
             <body className="overscroll-none w-full h-screen-small overflow-y-auto font-sans flex flex-col">
