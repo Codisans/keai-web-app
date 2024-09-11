53aa4ef3f5b42b4fa687965e8aa897ad8076b0b2
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-09-11 20:47:44 +0100

    fix: add browser scroll hack

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,21 @@
+'use client'
 import '@/app/global.css'
 import { AppContextProvider } from './AppContext'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { useEffect } from 'react'
 
 const RootLayout = ({ children }) => {
+    useEffect(() => {
+        window.scrollTo(0, 1)
+    }, [])
     return (
         <html className="overscroll-none select-none" lang="es">
-            <head>
-                <meta
-                    name="viewport"
-                    content="width=device-width, minimal-ui"
-                />
-            </head>
             <body className="overscroll-none w-full h-screen-small overflow-y-auto font-sans flex flex-col">
                 <SpeedInsights />
                 <AppContextProvider>{children}</AppContextProvider>
             </body>
         </html>
     )
-}
-
-export const metadata = {
-    title: 'KEAI',
 }
 
 export default RootLayout
