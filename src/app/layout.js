5aedbf081267716b7720101a967d183c9bfdaad0
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-10-07 03:18:58 +0100

    fix: reduce screen height

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const RootLayout = ({ children }) => {
     return (
-        <html className="overscroll-none select-none h-screen-large" lang="es">
+        <html className="overscroll-none select-none h-screen-small" lang="es">
             <body className="overscroll-none w-full h-screen-small overflow-y-auto font-sans flex flex-col">
                 <SpeedInsights />
                 <AppContextProvider>{children}</AppContextProvider>
