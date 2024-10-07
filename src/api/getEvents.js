41f31a52af726c6a62bdc20fec2a9c6258f4646a
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-10-07 03:44:56 +0100

    fix: amend test

--- src/api/getEvents.js
+++ src/api/getEvents.js
@@ -1,12 +1,28 @@
+// export const getEvents = async category => {
+//     const baseUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/events`
+//     const url = category ? `${baseUrl}/${category}` : baseUrl
+//     const res = await fetch(url)
+
+//     if (!res.ok) {
+//         // This will activate the closest `error.js` Error Boundary
+//         throw new Error('Failed to fetch data')
+//     }
+
+//     return res.json()
+// }
+
 export const getEvents = async category => {
     const baseUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/events`
     const url = category ? `${baseUrl}/${category}` : baseUrl
     const res = await fetch(url)
 
-    if (!res.ok) {
+    if (res.ok) {
+        console.log(res.ok)
+        return res.json()
         // This will activate the closest `error.js` Error Boundary
-        throw new Error('Failed to fetch data')
     }
 
-    return res.json()
+    console.log('error_____', res)
+
+    throw new Error('Failed to fetch data')
 }
