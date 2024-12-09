20dd1febea03bd31c97f0fa2cb526dbcefd50fca
Sebastian Strand <sebastian.strand@yoyodesign.com>
2024-12-09 21:15:45 +0000

    fix: update logos text

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -5,24 +5,7 @@ import {
     Logo2,
     Logo3,
 } from '@/components/atoms/Logo'
-import {
-    Aladin,
-    Grandstander,
-    Staatliches,
-    Yusei_Magic,
-} from 'next/font/google'
-
-const aladin = Aladin({
-    subsets: ['latin'],
-    weight: '400',
-    style: 'normal',
-})
-
-const grandstander = Grandstander({
-    subsets: ['latin'],
-    weight: '400',
-    style: 'normal',
-})
+import { Staatliches, Jockey_One, Squada_One, Teko } from 'next/font/google'
 
 const staatliches = Staatliches({
     subsets: ['latin'],
@@ -30,7 +13,19 @@ const staatliches = Staatliches({
     style: 'normal',
 })
 
-const yuseiMagic = Yusei_Magic({
+const jockeyOne = Jockey_One({
+    subsets: ['latin'],
+    weight: '400',
+    style: 'normal',
+})
+
+const squadaOne = Squada_One({
+    subsets: ['latin'],
+    weight: '400',
+    style: 'normal',
+})
+
+const teko = Teko({
     subsets: ['latin'],
     weight: '400',
     style: 'normal',
@@ -39,30 +34,63 @@ const yuseiMagic = Yusei_Magic({
 const TestPage = () => {
     return (
         <div className="w-full h-max flex flex-col items-center px-guter gap-20 py-header">
+            <div className="flex items-center justify-center flex-wrap gap-12">
+                <Logo className="[font-size:8rem;] font-tenorite" type="app" />
+                <Logo className="[font-size:8rem;] font-tenorite" />
+            </div>
+            <div className="flex items-center justify-center flex-wrap gap-12">
+                <Logo
+                    className={`[font-size:8rem;] ${staatliches.className}`}
+                    type="app"
+                />
+                <Logo
+                    className={`[font-size:8rem;] ${staatliches.className}`}
+                />
+            </div>
+            <div className="flex items-center justify-center flex-wrap gap-12 uppercase">
+                <Logo
+                    className={`[font-size:8rem;] ${jockeyOne.className}`}
+                    type="app"
+                />
+                <Logo className={`[font-size:8rem;] ${jockeyOne.className}`} />
+            </div>
+            <div className="flex items-center justify-center flex-wrap gap-12">
+                <Logo
+                    className={`[font-size:8rem;] ${jockeyOne.className}`}
+                    type="app"
+                />
+                <Logo className={`[font-size:8rem;] ${jockeyOne.className}`} />
+            </div>
+            <div className="flex items-center justify-center flex-wrap gap-12 uppercase">
+                <Logo
+                    className={`[font-size:8rem;] ${squadaOne.className}`}
+                    type="app"
+                />
+                <Logo className={`[font-size:8rem;] ${squadaOne.className}`} />
+            </div>
+            <div className="flex items-center justify-center flex-wrap gap-12">
+                <Logo
+                    className={`[font-size:8rem;] ${squadaOne.className}`}
+                    type="app"
+                />
+                <Logo className={`[font-size:8rem;] ${squadaOne.className}`} />
+            </div>
+            <div className="flex items-center justify-center flex-wrap gap-12 uppercase">
+                <Logo
+                    className={`[font-size:8rem;] ${teko.className}`}
+                    type="app"
+                />
+                <Logo className={`[font-size:8rem;] ${teko.className}`} />
+            </div>
+            <div className="flex items-center justify-center flex-wrap gap-12">
+                <Logo
+                    className={`[font-size:8rem;] ${teko.className}`}
+                    type="app"
+                />
+                <Logo className={`[font-size:8rem;] ${teko.className}`} />
+            </div>
             <AnimatedLogo2 background="gradient" className="w-64 h-64" />
             <Logo3 className="[font-size:8rem;]" />
-            <Logo className="[font-size:8rem;]" type="app" />
-            <Logo className="[font-size:8rem;]" />
-            <Logo
-                className={`[font-size:8rem;] ${yuseiMagic.className}`}
-                type="app"
-            />
-            <Logo className={`[font-size:8rem;] ${yuseiMagic.className}`} />
-            <Logo
-                className={`[font-size:8rem;] ${grandstander.className}`}
-                type="app"
-            />
-            <Logo className={`[font-size:8rem;] ${grandstander.className}`} />
-            <Logo
-                className={`[font-size:8rem;] ${aladin.className}`}
-                type="app"
-            />
-            <Logo className={`[font-size:8rem;] ${aladin.className}`} />
-            <Logo
-                className={`[font-size:8rem;] ${staatliches.className}`}
-                type="app"
-            />
-            <Logo className={`[font-size:8rem;] ${staatliches.className}`} />
             <AnimatedLogo className="w-64 h-64" />
             <AnimatedLogo background="collage" className="w-64 h-64" />
             <Logo2 className="[font-size:8rem;]" />
