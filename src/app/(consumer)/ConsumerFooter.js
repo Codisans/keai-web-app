9509b5733c47a3393e96a8a872794ff78a0dc9db
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-03-21 19:46:30 +0000

    fix: increase padding

--- src/app/(consumer)/ConsumerFooter.js
+++ src/app/(consumer)/ConsumerFooter.js
@@ -8,7 +8,7 @@ export const ConsumerFooter = () => {
     return (
         <footer className="sticky bottom-0 inset-x-0 z-40 pb-4 border-t border-grey-3 bg-white shrink">
             <nav>
-                <ul className="grid grid-cols-3 w-full items-center p-1 gap-x-1">
+                <ul className="grid grid-cols-3 w-full items-center px-gutter py-1 gap-x-1">
                     <li className="col-span-1">
                         <NavLink
                             className="button-icon w-full"
