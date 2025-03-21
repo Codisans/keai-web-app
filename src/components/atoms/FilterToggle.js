0afaaf9820dbb09e314371abcbb02596cc2ac857
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-03-21 22:31:56 +0000

    feat: improve filter toggle

--- src/components/atoms/FilterToggle.js
+++ src/components/atoms/FilterToggle.js
@@ -1,22 +1,30 @@
 'use client'
-import { Button } from '@/components/atoms/Button'
 import { usePathname, useSearchParams } from 'next/navigation'
 
 export const FilterToggle = ({ setIsOpen, isOpen }) => {
     const path = usePathname()
     const searchParams = useSearchParams()
 
+    const getIndicatorCount = searchParams => {
+        const priceFilter =
+            searchParams.get('min_price') || searchParams.get('max_price')
+                ? 1
+                : 0
+        const tagFilter =
+            Number(searchParams.getAll('tags[]')?.length) > 0 ? 1 : 0
+        return 1 + priceFilter + tagFilter
+    }
+
     return (
-        <Button
+        <button
             onClick={() => setIsOpen(s => !s)}
-            className="w-min relative border-grey-3 border rounded-ui"
-            style="icon"
+            type="button"
+            className={`button-icon relative ${isOpen ? 'current' : ''}`}
             disabled={path
                 .split('/')
-                ?.some(x => ['favoritos', 'perfil', 'cuenta'].includes(x))}
-            active={isOpen}>
-            <span className="text-indicator absolute top-0 right-0 bg-black text-white rounded-full h-4 w-4 flex items-center justify-center">
-                {searchParams.size}
+                ?.some(x => ['favoritos', 'perfil', 'cuenta'].includes(x))}>
+            <span className="typo-small absolute -top-1 -right-1 bg-white text-black font-bold rounded-full h-4 w-4 flex items-center justify-center">
+                {getIndicatorCount(searchParams)}
             </span>
             <svg
                 className="w-6 h-6"
@@ -26,6 +34,6 @@ export const FilterToggle = ({ setIsOpen, isOpen }) => {
                 viewBox="0 0 24 24">
                 <path d="M10.83 5a3.001 3.001 0 0 0-5.66 0H4a1 1 0 1 0 0 2h1.17a3.001 3.001 0 0 0 5.66 0H20a1 1 0 1 0 0-2h-9.17ZM4 11h9.17a3.001 3.001 0 0 1 5.66 0H20a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H4a1 1 0 1 1 0-2Zm1.17 6H4a1 1 0 1 0 0 2h1.17a3.001 3.001 0 0 0 5.66 0H20a1 1 0 1 0 0-2h-9.17a3.001 3.001 0 0 0-5.66 0Z" />
             </svg>
-        </Button>
+        </button>
     )
 }
