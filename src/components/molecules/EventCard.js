45d765f156832ba2fc362d21eedbc45ab44dedd9
Sebastian Strand <sebastian.strand@yoyodesign.com>
2025-03-23 15:16:25 +0000

    save wip

--- src/components/molecules/EventCard.js
+++ src/components/molecules/EventCard.js
@@ -1,24 +1,37 @@
 import { DateTime } from '../atoms/DateTime'
 import Link from 'next/link'
+import { SaveEventButton } from '../atoms/SaveEventButton'
 
 export const EventCard = ({ event, type = 'carousel' }) => {
     if (type === 'carousel')
         return (
-            <div className="flex flex-col gap-y-3 relative w-full">
+            <div className="flex flex-col gap-y-1 relative w-full">
                 <div className="w-full aspect-[16/9] relative rounded-card overflow-hidden">
                     <img
                         className="absolute inset-0 w-full h-full object-cover"
-                        src={event.cover || '/placeholder.jpg'}
+                        src={event.cover_url || '/placeholder.jpg'}
                     />
                 </div>
-                <div className="">
-                    <h3>{event.name}</h3>
-                    <p>{event.date}</p>
-                    <p>Desde: $ {event.price}</p>
+                <div className="flex flex-col">
+                    <h3 className="typo-h5">{event.name}</h3>
+                    <ul className="flex flex-wrap gap-x-2">
+                        {/* {event.tags.map(tag => (
+                            <li key={tag} className="tag">
+                                {tag}
+                            </li>
+                        ))} */}
+                    </ul>
+                    <DateTime className="typo-caps" date={event.start_date} />
+
+                    {/* <p className="typo-caps">Desde: $ {event.price}</p> */}
                 </div>
                 <Link
                     href={`/evento/${event.id}`}
                     className="absolute inset-0"></Link>
+                <SaveEventButton
+                    className="absolute top-1 z-10 right-1"
+                    eventId={event.id}
+                />
             </div>
         )
 
@@ -28,17 +41,21 @@ export const EventCard = ({ event, type = 'carousel' }) => {
                 <div className="col-span-4 aspect-[16/9] relative rounded-card overflow-hidden">
                     <img
                         className="absolute inset-0 w-full h-full object-cover group-hover/card:scale-105 transition-transform ease-in-out duration-500"
-                        src={event.cover}
+                        src={event.cover_url || '/placeholder.jpg'}
                     />
                 </div>
                 <div className="col-span-8 flex flex-col">
                     <h3 className="typo-h4">{event.name}</h3>
                     <DateTime date={event.start_date} />
-                    <p className="typo-small">${event.price}</p>
+                    {/* <p className="typo-small">${event.price}</p> */}
                 </div>
                 <Link
                     href={`/evento/${event.id}`}
                     className="absolute inset-0"></Link>
+                <SaveEventButton
+                    className="absolute top-1 z-10 right-1"
+                    eventId={event.id}
+                />
             </div>
         )
 }
