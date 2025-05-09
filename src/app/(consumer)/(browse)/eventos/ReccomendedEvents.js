8f3325b500c3a413006a6a09e145d9ad0da35121
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-05-09 19:36:41 -0400

    feat: order recomended tags

--- src/app/(consumer)/(browse)/eventos/ReccomendedEvents.js
+++ src/app/(consumer)/(browse)/eventos/ReccomendedEvents.js
@@ -3,16 +3,28 @@
 import { useApi } from '@/hooks/api'
 import { useEffect, useState } from 'react'
 import { EventCarousel } from './EventCarousel'
+import { useUserTags } from '@/hooks/userTags'
 
 export const ReccomendedEvents = () => {
-    const { events } = useApi()
     const [items, setItems] = useState([])
+    const { userTags } = useUserTags()
+    const { events } = useApi()
 
     useEffect(() => {
-        if (!events) return
+        if (!userTags || !events) return
 
-        setItems(events?.slice(0, 20))
-    }, [events])
+        const tagIds = userTags.map((tag) => tag.id)
+        const filteredEvents = events.filter((event) => tagIds.some((id) => event.tags.some((t) => t.id === id)))
+        const sortedEvents = filteredEvents.sort((a, b) => {
+            const aTags = a.tags.filter((tag) => tagIds.includes(tag.id))
+            const bTags = b.tags.filter((tag) => tagIds.includes(tag.id))
+            const aIntersectionCount = aTags.length
+            const bIntersectionCount = bTags.length
+            return bIntersectionCount - aIntersectionCount
+        })
+
+        setItems(sortedEvents?.slice(0, 20))
+    }, [userTags, events])
 
     return <EventCarousel heading="Recomendados para ti" events={items} />
 }
