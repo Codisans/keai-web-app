35c4de2233ef4a691d02e54b99c94553a6e89030
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-05-12 22:43:36 -0400

    fix: amend spacing

--- src/app/(consumer)/(browse)/eventos/layout.js
+++ src/app/(consumer)/(browse)/eventos/layout.js
@@ -6,7 +6,7 @@ const EventsLayout = ({ children }) => {
         <>
             <EventsHeader />
             <div className="grow">
-                <FilterForm className="open:block max-[496px]:left-2 min-[496px]:w-full right-2 hidden fixed z-header top-[7.3rem] border border-grey bottom-[3.6rem] rounded overflow-hidden select-none" />
+                <FilterForm className="open:block max-[496px]:left-2 min-[496px]:w-full right-2 hidden fixed z-header top-[7.3rem] border border-grey bottom-[4.6rem] rounded overflow-hidden select-none" />
                 <div className='w-full overflow-hidden'>{children}</div>
             </div>
         </>
