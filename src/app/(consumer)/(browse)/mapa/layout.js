aa9ddbf33fe4c164a4ad65c38a4ad5822e9a3f7c
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-05-12 22:40:52 -0400

    fix: amend footer

--- src/app/(consumer)/(browse)/mapa/layout.js
+++ src/app/(consumer)/(browse)/mapa/layout.js
@@ -7,7 +7,7 @@ const MapLayout = ({ children }) => {
     return (
         <div className="w-full flex flex-col grow">
             <MapHeader />
-            <FilterForm isMap={true} className="open:block max-[496px]:left-2 min-[496px]:w-full right-2 hidden fixed z-header top-[6.8rem] border border-grey bottom-[4.1rem] rounded overflow-hidden select-none" />
+            <FilterForm isMap={true} className="open:block max-[496px]:left-2 min-[496px]:w-full right-2 hidden fixed z-header top-[6.8rem] border border-grey bottom-[3.6rem] rounded overflow-hidden select-none" />
 
             <main className="w-full h-full grow relative flex">
                 <Map>{children}</Map>
--- src/app/(consumer)/ConsumerFooter.js
+++ src/app/(consumer)/ConsumerFooter.js
@@ -6,21 +6,21 @@ import { NavLink } from '@/components/atoms/NavLink'
 
 export const ConsumerFooter = () => {
     return (
-        <footer className="sticky bottom-0 inset-x-0 z-40 py-2 border-t border-grey bg-white shrink">
+        <footer className="sticky bottom-0 inset-x-0 z-40 py-2 pb-4 border-t border-grey bg-white shrink">
             <nav>
-                <ul className="flex justify-center w-full items-center px-gutter gap-x-8">
+                <ul className="flex justify-center w-full items-center px-gutter gap-x-4">
                     <li>
-                        <NavLink className="button-icon w-20" href="/mapa">
+                        <NavLink className="button-icon w-24" href="/mapa">
                             <MapIcon />
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink className="button-icon w-20" href="/eventos">
+                        <NavLink className="button-icon w-24" href="/eventos">
                             <HomeIcon />
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink className="button-icon w-20" href="/perfil">
+                        <NavLink className="button-icon w-24" href="/perfil">
                             <PersonIcon />
                         </NavLink>
                     </li>
