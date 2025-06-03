a257e5a59b4041a90abc75ec95e6b426a076d4da
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-03 23:15:25 +0100

    fix: amend async functions

--- src/app/(consumer)/(browse)/eventos/EventsHeader.js
+++ src/app/(consumer)/(browse)/eventos/EventsHeader.js
@@ -1,14 +1,11 @@
 import { FilterToggle } from '../FilterToggle'
-import api from '@/lib/api'
 import { SearchInput } from '../SearchInput'
 import { CategoryBar } from '../CategoryBar'
 
-export const EventsHeader = async () => {
-    const categories = await api.getCategories()
-
+export const EventsHeader = () => {
     return (
         <header className="sticky top-0 inset-x-0 shadow z-header bg-white shrink listing">
-            <CategoryBar categories={categories} view="listing" />
+            <CategoryBar view="listing" />
             <div className="flex gap-2 px-2 pb-2">
                 <SearchInput />
                 <FilterToggle />
--- src/app/(consumer)/(browse)/mapa/MapHeader.js
+++ src/app/(consumer)/(browse)/mapa/MapHeader.js
@@ -1,13 +1,11 @@
 import { FilterToggle } from '../FilterToggle'
 import { SearchInput } from '../SearchInput'
 import { CategoryBar } from '../CategoryBar'
-import api from '@/lib/api'
 
-export const MapHeader = async () => {
-    const categories = await api.getCategories()
+export const MapHeader = () => {
     return (
         <header className="fixed z-header inset-x-0 top-0 pointer-events-none map">
-            {categories && <CategoryBar categories={categories} view="map" />}
+            <CategoryBar view="map" />
             <div className="px-2 flex gap-2 pb-2">
                 <SearchInput />
                 <FilterToggle />
