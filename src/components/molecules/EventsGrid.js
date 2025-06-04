73dc0096989cab2a93dbe237c32310d04f937cd6
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-04 10:46:49 +0100

    fix: remove unused files

--- src/components/molecules/EventsGrid.js
+++
@@ -1,9 +0,0 @@
-const EventsGrid = props => {
-    const { className = '', children } = props
-
-    return (
-        <ul className={`grid grid-cols-12 gap-6 ${className}`}>{children}</ul>
-    )
-}
-
-export default EventsGrid
