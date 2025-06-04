33316db49164b4bb940b05f3c54a958c22ef9ef9
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-04 21:27:28 +0100

    fix: amend layout

--- src/app/(consumer)/(browse)/eventos/layout.js
+++ src/app/(consumer)/(browse)/eventos/layout.js
@@ -5,9 +5,11 @@ const EventsLayout = ({ children }) => {
     return (
         <>
             <EventsHeader />
-            <div className="grow">
+            <div className="grow flex">
                 <FilterForm className="open:block max-[496px]:left-2 min-[496px]:w-full right-2 hidden fixed z-header top-28 border border-grey bottom-18 rounded overflow-hidden select-none" />
-                <div className='w-full overflow-hidden bg-white-true'>{children}</div>
+                <div className="w-full overflow-hidden bg-white-true">
+                    {children}
+                </div>
             </div>
         </>
     )
