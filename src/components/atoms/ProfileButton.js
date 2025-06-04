c1668d7ff157cbed19f521c6572881c847a2f814
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-04 08:28:26 +0100

    fix: update profile button

--- src/components/atoms/ProfileButton.js
+++ src/components/atoms/ProfileButton.js
@@ -1,20 +1,15 @@
 'use client'
 
-import { NavLink } from "./NavLink"
-import LoginIcon from '@mui/icons-material/Login'
+import { NavLink } from './NavLink'
 import PersonIcon from '@mui/icons-material/Person'
-import { useAuth } from "@/hooks/auth"
+import { useAuth } from '@/hooks/auth'
 
 export const ProfileButton = ({ ...props }) => {
     const { user } = useAuth()
 
-    return user ? (        
-            <NavLink {...props} href="/perfil">
-                <PersonIcon />
-            </NavLink> 
-        ) : ( 
-            <NavLink {...props} href="/entrar">
-                <LoginIcon />
-            </NavLink>
-        )
+    return (
+        <NavLink {...props} href={user ? '/perfil' : '/entrar'}>
+            <PersonIcon />
+        </NavLink>
+    )
 }
