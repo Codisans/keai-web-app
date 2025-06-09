eb407b1c65c2e472b208ee90f9fe6375a947b774
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-09 10:01:30 +0100

    fix: amend upcoming events for prfile

--- src/app/(consumer)/(profile)/perfil/UpcomingEvents.js
+++ src/app/(consumer)/(profile)/perfil/UpcomingEvents.js
@@ -11,7 +11,7 @@ export const UpcomingEvents = () => {
 
     useEffect(() => {
         const filteredEvents = userEvents
-            ?.filter(e => moment(e.end_date).isAfter(moment()))
+            ?.filter(e => moment(e.start_date).isAfter(moment()))
             .sort((a, b) => moment(a.start_date).diff(moment(b.start_date)))
         setEvents(filteredEvents)
     }, [userEvents])
@@ -22,7 +22,12 @@ export const UpcomingEvents = () => {
                 <li
                     className="w-full border-t border-grey py-grid-gap first:border-t-0"
                     key={i}>
-                    <EventCard event={event} type="list" remove={true} showTags={false} />
+                    <EventCard
+                        event={event}
+                        type="list"
+                        remove={true}
+                        showTags={false}
+                    />
                 </li>
             ))}
         </ul>
