032a129912702b84e628d7ecd9e5750ccb3d341a
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-10 21:43:35 +0100

    fix: amend date logic

--- src/app/(consumer)/(browse)/DateRadio.js
+++ src/app/(consumer)/(browse)/DateRadio.js
@@ -9,7 +9,7 @@ export const DateRadio = ({ date, setDate }) => {
     const today = moment().format('YYYY-MM-DD')
     const isWeekend = moment().weekday() >= 5 || moment().weekday() == 0
     const startOfWeek = moment().startOf('week')
-    const isSunday = moment().weekday() == 0
+    const isSunday = moment().weekday() === 0
 
     const baseOptions = [
         {
@@ -27,7 +27,7 @@ export const DateRadio = ({ date, setDate }) => {
         },
     ]
 
-    const weekendOption =  {
+    const weekendOption = {
         id: 'this-weekend',
         label: 'Este FDS',
         value: [
@@ -38,55 +38,58 @@ export const DateRadio = ({ date, setDate }) => {
         ],
     }
 
-    const weekOption =
-        isWeekend
-            ? {
-                  id: 'next-week',
-                  label: 'Próx. semana',
-                  value: [
-                      moment().day(0).add(1, 'day').format('YYYY-MM-DD'),
-                      moment().day(7).format('YYYY-MM-DD'),
-                  ],
-              }
-            : {
-                  id: 'this-week',
-                  label: 'Esta semana',
-                  value: [
-                      moment().format('YYYY-MM-DD'),
-                      moment().day(7).format('YYYY-MM-DD'),
-                  ],
-              }
+    const weekOption = isWeekend
+        ? {
+              id: 'next-week',
+              label: 'Próx. semana',
+              value: [
+                  moment().day(0).add(1, 'day').format('YYYY-MM-DD'),
+                  moment().day(7).format('YYYY-MM-DD'),
+              ],
+          }
+        : {
+              id: 'this-week',
+              label: 'Esta semana',
+              value: [
+                  moment().format('YYYY-MM-DD'),
+                  moment().day(7).format('YYYY-MM-DD'),
+              ],
+          }
 
-    const options = isSunday ? [...baseOptions, weekOption] : [...baseOptions, weekendOption, weekOption]
+    const options = isSunday
+        ? [...baseOptions, weekOption]
+        : [...baseOptions, weekendOption, weekOption]
 
     return (
         <div className="flex flex-col gap-2">
             <div className="w-full grid grid-cols-5 items-center gap-2 typo-caps">
-                {date[0] == today && date[1] == null
-                    ? 
-                        <span className='col-span-full'>Fechas: TODAS</span>
-                    :
-                        <>
-                            {date[0] != '' && (
-                                <span className='col-span-2'>
-                                    <DateTime date={date[0]} format='ddd D MMM' />
+                {date[0] == today && date[1] == null ? (
+                    <span className="col-span-full">Fechas: TODAS</span>
+                ) : (
+                    <>
+                        {date[0] != '' && (
+                            <span className="col-span-2">
+                                <DateTime date={date[0]} format="ddd D MMM" />
+                            </span>
+                        )}
+                        {date[1] != '' && date[0] != date[1] && (
+                            <>
+                                <span className="col-span-1">
+                                    <Symbol
+                                        className="w-6 h-6 rotate-45 -my-2 mx-auto"
+                                        name="arrows-horizontal"
+                                    />
                                 </span>
-                            )}
-                            {date[1] != '' && date[0] != date[1] && (
-                                <>
-                                    <span className='col-span-1'>
-                                        <Symbol
-                                            className="w-6 h-6 rotate-45 -my-2 mx-auto"
-                                            name="arrows-horizontal"
-                                        />
-                                    </span>
-                                    <span className='col-span-2 text-right'>
-                                    <DateTime date={date[1]} format='ddd D MMM' />
-                                    </span>
-                                </>
-                            )}
-                        </>
-                }
+                                <span className="col-span-2 text-right">
+                                    <DateTime
+                                        date={date[1]}
+                                        format="ddd D MMM"
+                                    />
+                                </span>
+                            </>
+                        )}
+                    </>
+                )}
             </div>
             <div className="flex flex-wrap gap-2">
                 {options.map((option, index) => (
