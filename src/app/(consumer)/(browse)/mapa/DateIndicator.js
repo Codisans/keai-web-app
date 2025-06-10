406afa2bdb0c79d9af3a29e85da12e46e837a0da
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-10 21:42:44 +0100

    fix: update map date indicator

--- src/app/(consumer)/(browse)/mapa/DateIndicator.js
+++ src/app/(consumer)/(browse)/mapa/DateIndicator.js
@@ -10,7 +10,7 @@ export const DateIndicator = () => {
     const maxDate = searchParams.get('max_date') || defaultMapDate()[1]
 
     return (
-        <div className="absolute bottom-gutter right-gutter z-20 flex gap-x-0.5 pointer-events-none bg-white-true rounded p-1 items-center border border-grey gap-1">
+        <div className="absolute bottom-gutter right-gutter z-20 flex gap-x-2 h-10 pointer-events-none bg-white-true rounded px-3 items-center border border-grey gap-1">
             {minDate !== maxDate && (
                 <>
                     <DateCard date={minDate} />
@@ -24,10 +24,8 @@ export const DateIndicator = () => {
 
 export const DateCard = ({ date, className = '' }) => {
     return (
-        <div
-            className={`flex flex-col gap-y-1 items-center typo-button text-sm px-0.5 ${className}`}>
-            <span>{moment(date).format('DD')}</span>
-            <span>{moment(date).format('MMM')}</span>
+        <div className={`text-nowrap typo-button text-xs ${className}`}>
+            {moment(date).format('ddd')} {moment(date).format('DD')}
         </div>
     )
 }
