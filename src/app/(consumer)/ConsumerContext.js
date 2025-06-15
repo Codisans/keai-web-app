348cca41e43192b93723f40581cc5ea4964f2d41
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-15 10:35:21 -0400

    fix: remove unsused states

--- src/app/(consumer)/ConsumerContext.js
+++ src/app/(consumer)/ConsumerContext.js
@@ -3,25 +3,20 @@ import { usePathname } from 'next/navigation'
 import { createContext, useState, useEffect, useContext } from 'react'
 
 export const ConsumerContext = createContext({
-    categories: [],
-    tags: [],
     filterIsOpen: false,
     setFilterIsOpen: () => {},
     menuIsOpen: false,
     setMenuIsOpen: () => {},
-    selectedCategory: null,
-    setSelectedCategory: () => {},
     selectedEvent: null,
     setSelectedEvent: () => {},
 })
 
-export const ConsumerContextProvider = ({ children, categories, tags }) => {
+export const ConsumerContextProvider = ({ children }) => {
     const path = usePathname()
     const [filterIsOpen, setFilterIsOpen] = useState(false)
     const [highlightSearchButton, setHighlightSearchButton] = useState(false)
     const [menuIsOpen, setMenuIsOpen] = useState(false)
     const [selectedEvent, setSelectedEvent] = useState(null)
-    const [selectedCategory, setSelectedCategory] = useState(null)
 
     const openFilter = () => {
         setFilterIsOpen(true)
@@ -35,24 +30,15 @@ export const ConsumerContextProvider = ({ children, categories, tags }) => {
     useEffect(() => {
         setMenuIsOpen(false)
         closeFilter()
-        setSelectedCategory(
-            path.includes('/mapa/') || path.includes('/eventos/')
-                ? path.split('/').pop()
-                : null,
-        )
     }, [path])
 
     return (
         <ConsumerContext.Provider
             value={{
-                categories,
-                tags,
                 filterIsOpen,
                 setFilterIsOpen,
                 menuIsOpen,
                 setMenuIsOpen,
-                selectedCategory,
-                setSelectedCategory,
                 selectedEvent,
                 setSelectedEvent,
                 highlightSearchButton,
--- src/app/(consumer)/layout.js
+++ src/app/(consumer)/layout.js
@@ -1,14 +1,10 @@
 import { Suspense } from 'react'
 import { ConsumerContextProvider } from './ConsumerContext'
-import api from '@/lib/api'
 import { Loading } from '@/components/atoms/Loading'
 
-const ConsumerLayout = async ({ children }) => {
-    const categories = await api.getCategories()
-    const tags = await api.getTags()
-
+const ConsumerLayout = ({ children }) => {
     return (
-        <ConsumerContextProvider categories={categories} tags={tags}>
+        <ConsumerContextProvider>
             <Suspense fallback={<Loading />}>{children}</Suspense>
         </ConsumerContextProvider>
     )
