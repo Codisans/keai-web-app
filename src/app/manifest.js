38d281700af81cd4c7101455530d0132d3b83423
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-17 19:39:09 -0400

    fix: update manifest

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,12 +1,13 @@
 export default function manifest() {
     return {
-        name: 'Keai App',
+        name: 'Keai',
         short_name: 'Keai',
-        description: 'Keai App',
-        start_url: '/',
+        description:
+            'Descubre la vida de tu ciudad. Encuentra panoramas y enterate de todo lo que pasa cerca de ti.',
+        start_url: '/eventos',
         display: 'standalone',
-        background_color: '#fff',
-        theme_color: '#fff',
+        background_color: '#f7f7f7',
+        theme_color: '#f7f7f7',
         icons: [
             {
                 src: '/favicon.ico',
