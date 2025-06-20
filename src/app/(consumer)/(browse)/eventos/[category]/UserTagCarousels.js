b01a1e76e26cdf4a66327c12bc8f53070ff2c235
Sebastian Strand <sebastianstrand@Sebastians-MacBook-Pro.local>
2025-06-19 22:02:20 -0400

    fix: add dummy tag carousels for categories

--- src/app/(consumer)/(browse)/eventos/[category]/UserTagCarousels.js
+++ src/app/(consumer)/(browse)/eventos/[category]/UserTagCarousels.js
@@ -1,23 +1,36 @@
 'use client'
 
-import { useUserTags } from "@/hooks/userTags"
-import { EventCarouselQuery } from "../EventCarouselQuery"
-import { useEffect, useState } from "react"
+import { useUserTags } from '@/hooks/userTags'
+import { EventCarouselQuery } from '../EventCarouselQuery'
+import { useEffect, useState } from 'react'
 import moment from 'moment'
+import { useApi } from '@/hooks/api'
 
-export const UserTagCarousels = ({category}) => {
+export const UserTagCarousels = ({ category }) => {
     const { userTags } = useUserTags()
-    const [tags, setTags] = useState([])
+    const { tags } = useApi()
+    const [carouselTags, setCarouselTags] = useState([])
 
     useEffect(() => {
-        if (!userTags) return
-        setTags(userTags)
-    }, [userTags])
-    
-    return tags?.map((tag, i) => {
+        if (!userTags && !tags) return
+        if (userTags) {
+            setCarouselTags(userTags)
+            return
+        }
+        setCarouselTags(tags.filter(() => Math.random() > 0.5))
+    }, [userTags, tags])
+
+    return carouselTags?.map((tag, i) => {
         const searchParams = new URLSearchParams()
         searchParams.set('min_date', moment().format('YYYY-MM-DD'))
         searchParams.set('categories[]', category)
         searchParams.set('tags[]', tag.id)
-        return <EventCarouselQuery heading={tag.name} key={i} searchParams={searchParams} />})
+        return (
+            <EventCarouselQuery
+                heading={tag.name}
+                key={i}
+                searchParams={searchParams}
+            />
+        )
+    })
 }
